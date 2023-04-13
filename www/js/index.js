f42ba93754566adb6ee7ee896dc971c38a18eb66
RobertoCavallero <roberto.cavallero2003@gmail.com>
2023-04-13 23:09:16 +0200

    made progress in the project

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,29 @@
-$(()=>{
-    $("#btnStart").on("click",()=>{
-        
+$(() => {
+    $("#playImg").on("click", () => {
+        $("#startSection").hide();
+        /*Camera Plugin Code*/
+        //Configuro le opzioni di getPicture
+        let opzioni = {
+            sourceType: Camera.PictureSourceType.CAMERA,
+            destinationType: Camera.DestinationType.DATA_URL,
+            quality: 50
+        };
+        navigator.camera.getPicture(cameraSuccess, cameraError, opzioni);
+
+
+        function cameraSuccess(imageData) {
+            /*var image = document.getElementById('imgMia'); //<img> nel html
+            image.src = "data:image/jpeg;base64," + imageData;*/
+            $("#imgContainer").css({ 'background-image': "url(data:image/jpeg;base64," + imageData + ')', 'border': "solid 5px black" })
+
+            $("#imgContainer").draggable({
+                axis: "x",
+                containment: "window"
+            });
+        }
+        //Richiamata in caso di errori : il mancato scatto di una foto o la mancata selezione
+        function cameraError(mesErrore) {
+            alert("Errore: " + mesErrore);
+        }
     })
 })
